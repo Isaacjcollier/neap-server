bc0686f54403a875709ebedc1e8bc6a6b6e6f4b6
Isaacjcollier <isaacjcollier@gmail.com>
2016-10-24 13:23:08 -0600

    refactor

--- src/server/config/main-config.js
+++ src/server/config/main-config.js
@@ -23,18 +23,17 @@
     }
 
     // *** cross domain requests *** //
-        const allowCrossDomain = function(req, res, next) {
-          res.header('Access-Control-Allow-Origin', '*');
-          // res.header('Access-Control-Allow-Methods', 'GET ,PUT, POST, DELETE');
-          // res.header('Access-Control-Allow-Headers', 'Content-Type');
-          next();
-        };
-
-        app.use(allowCrossDomain);
+    const allowCrossDomain = function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      // res.header('Access-Control-Allow-Methods', 'GET ,PUT, POST, DELETE');
+      // res.header('Access-Control-Allow-Headers', 'Content-Type');
+      next();
+    };
 
     app.use(cookieParser());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(allowCrossDomain);
     app.use(flash());
 
   };
